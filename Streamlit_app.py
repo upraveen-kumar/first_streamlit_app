077e593a1b0b00766dfa5b2c6570b156d3b00859
upraveen-kumar <78647462+upraveen-kumar@users.noreply.github.com>
2022-04-16 14:14:39 +0530

    Update Streamlit_app.py
    import a txt file into a dataframe. display the data frame.

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('My Mother\'s New Diner Menu');
 streamlit.header('Breakfast Menu');
 streamlit.text('🥣 Omega 3 & blueberry oatmeal');
@@ -7,3 +9,5 @@ streamlit.text('🐔 hard boiled free range egg');
 streamlit.text('🥑🍞 avocado toast');
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+streamlit.dataframe(my_fruit_list);
