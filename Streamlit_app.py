abbd7c6f9b2b6730c4b8818d645639d412052754
upraveen-kumar <78647462+upraveen-kumar@users.noreply.github.com>
2022-04-16 14:09:21 +0530

    Update Streamlit_app.py
    rite now, just copy pasted emojis. let's c if that works! don't know of any other way.

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('My Mother\'s New Diner Menu');
 streamlit.header('Breakfast Menu');
-streamlit.text('Omega 3 & blueberry oatmeal');
-streamlit.text('kale, spinach & rocket smootie');
-streamlit.text('hard boiled free range egg');
+streamlit.text('🥣 Omega 3 & blueberry oatmeal');
+streamlit.text('🥗 kale, spinach & rocket smootie');
+streamlit.text('🐔 hard boiled free range egg');
+streamlit.text('🥑🍞 avocado toast');
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
