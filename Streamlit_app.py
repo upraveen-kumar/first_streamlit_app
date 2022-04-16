40e81053da64632035a66f952e200b58751eea1d
upraveen-kumar <78647462+upraveen-kumar@users.noreply.github.com>
2022-04-16 14:02:32 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Mother\'s New Diner Menu');
 streamlit.title('Breakfast Menu');
+streamlit.title('Omega 3 & blueberry oatmeal');
+streamlit.title('kale, spinach & rocket smootie');
+streamlit.title('hard boiled free range egg');
