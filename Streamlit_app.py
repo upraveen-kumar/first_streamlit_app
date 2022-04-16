91aff57ba120280e30b45549e31e089fe04569da
upraveen-kumar <78647462+upraveen-kumar@users.noreply.github.com>
2022-04-16 14:04:58 +0530

    Update Streamlit_app.py
    made changes after luking @ snowflake sol.

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Mother\'s New Diner Menu');
-streamlit.title('Breakfast Menu');
-streamlit.title('Omega 3 & blueberry oatmeal');
-streamlit.title('kale, spinach & rocket smootie');
-streamlit.title('hard boiled free range egg');
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & blueberry oatmeal');
+streamlit.text('kale, spinach & rocket smootie');
+streamlit.text('hard boiled free range egg');
