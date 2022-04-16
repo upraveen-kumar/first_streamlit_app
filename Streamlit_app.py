507cbb47e412c1f74a975c473a5a39d07f181669
upraveen-kumar <78647462+upraveen-kumar@users.noreply.github.com>
2022-04-16 14:22:50 +0530

    Update Streamlit_app.py
    added code to set the list index 2fruit name,instead of number (0 based counting). makes more sense 2select 4ppl

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -10,6 +10,8 @@ streamlit.text('🥑🍞 avocado toast');
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+#instead of giving index by nbr, give it a name i.e in the multi select below, index is now fruit name (1st col of csv file), instead of position!
+my_fruit_list = my_fruit_list.set_index('Fruit');
 
 #multi select list -- users can pick their own fruit combo
 streamlit.multiselect('pick ur choice of fruits:', list(my_fruit_list.index));
